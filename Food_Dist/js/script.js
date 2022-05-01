cf816a96dea11c43cea10704035d71fa1cb83fde
Pavel Mezencev <mezencev24@mail.ru>
2022-05-01 11:40:56 +0300

    add promo timer

--- Food_Dist/js/script.js
+++ Food_Dist/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+  ///////// TABS ///////////////////////
   const tabsContent = document.querySelectorAll('.tabcontent');
   const tabs = document.querySelectorAll('.tabheader__item');
   const tabsParent = document.querySelector('.tabheader__items');
@@ -35,4 +36,60 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  ///////// TIMER ///////////////////////
+  const deadline = '2022-05-01T11:41:00';
+  setTimer('.timer', deadline);
+  console.log(getDeadlineRemaining(deadline));
+
+  function getDeadlineRemaining(endtime) {
+    const remaining = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(remaining / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((remaining / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((remaining / (1000 * 60)) % 60),
+      seconds = Math.floor((remaining / 1000) % 60);
+
+    return {
+      remaining,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function addZero(num) {
+    if (num <= 0 || num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setTimer(selector, endtime) {
+    const timerEl = document.querySelector(selector),
+      daysEl = timerEl.querySelector('#days'),
+      hoursEl = timerEl.querySelector('#hours'),
+      minutesEl = timerEl.querySelector('#minutes'),
+      secondsEl = timerEl.querySelector('#seconds'),
+      timerId = setInterval(displayTimer, 1000);
+
+    displayTimer();
+
+    function displayTimer() {
+      const t = getDeadlineRemaining(endtime);
+      if (t.remaining <= 0) {
+        clearInterval(timerId);
+        daysEl.innerHTML = addZero(0);
+        hoursEl.innerHTML = addZero(0);
+        minutesEl.innerHTML = addZero(0);
+        secondsEl.innerHTML = addZero(0);
+      } else {
+        daysEl.innerHTML = addZero(t.days);
+        hoursEl.innerHTML = addZero(t.hours);
+        minutesEl.innerHTML = addZero(t.minutes);
+        secondsEl.innerHTML = addZero(t.seconds);
+      }
+    }
+  }
 });

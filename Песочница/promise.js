3c921064137a418d7827f93e777312146cbf096f
Pavel Mezencev <mezencev24@mail.ru>
2022-05-02 20:45:08 +0300

    work with promise

---
+++ Песочница/promise.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// const delay = (time) => new Promise((res) => setTimeout(() => res(), time));
+
+// delay(1000).then(() => console.log('after 1 sec'));
+
+// Promise.race([delay(1000), delay(4000)]).then(() => console.log('race'));
+// Promise.all([delay(1000), delay(4000)]).then(() => console.log('all'));
